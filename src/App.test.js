cea52ef9a5691e15716ccf9c18d8f2d3276c3b1e
SATHISH KUMAR S <114249427+Sathish288@users.noreply.github.com>
2026-02-24 11:53:56 +0530

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders homepage', () => {
-  render(<App />);
-  const element = screen.getByText(/home/i); 
+  render(
+    <MemoryRouter initialEntries={['/ecommerce']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  const element = screen.getByText(/home/i); // change if needed
   expect(element).toBeInTheDocument();
 });
