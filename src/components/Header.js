c4dfd0f799a84d5d501f90c70de450f65f36bb52
SATHISH KUMAR S <114249427+Sathish288@users.noreply.github.com>
2026-02-25 11:08:06 +0530

    Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = ({ cartItemCount }) => {
   return (
-    <header>
+    <header className="main-header">
       <div className="header-container">
-        <h1>E-Commerce Site</h1>
+        
+        {/* Logo Section */}
+        <div className="logo">
+          <Link to="/">
+            <span className="logo-icon">🛍</span>
+            <span className="logo-text">ShopNest</span>
+          </Link>
+        </div>
+
+        {/* Navigation */}
         <nav>
-          <ul>
+          <ul className="nav-links">
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-            <li><Link to="/order">Order</Link></li>
-            <li className="cart">
-              <Link to="/cart">Cart</Link>
-              {cartItemCount > 0 && <span className="cart-count">{cartItemCount}</span>}
-            </li>
+            <li><Link to="/order">Orders</Link></li>
           </ul>
         </nav>
-        <div className="search-bar">
-          <input type="text" placeholder="Search products..." />
+
+        {/* Right Section */}
+        <div className="header-right">
+          <input 
+            type="text" 
+            placeholder="Search products..." 
+            className="search-input"
+          />
+
+          <Link to="/cart" className="cart-icon">
+            <FaShoppingCart size={20} />
+            {cartItemCount > 0 && (
+              <span className="cart-count">{cartItemCount}</span>
+            )}
+          </Link>
         </div>
+
       </div>
     </header>
   );
